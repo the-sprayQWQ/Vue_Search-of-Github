f815521b9dd84f409b3a391eea1a2483cb34acf3
the-sprayQWQ <spary1678@outlook.com>
2022-08-01 12:25:49 +0800

    入口文件以及组件汇总
    放入SRC目录下

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  <div class="container">
+    <search />
+    <list />
+  </div>
+</div>
+</template>
+
+<script>
+    import Search from './components/Search'
+    import List from './components/List'
+export default {
+    components:{ Search,List }
+
+}
+</script>
+
+<style>
+
+</style>
